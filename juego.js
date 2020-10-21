e8e7e767ec1b477ef51ce902d974dc0cc54d5a0c
Alber321 <lsolis2@ucol.mx>
2020-10-21 18:15:03 -0500

    avance 3

--- juego.js
+++ juego.js
@@ -1,7 +1,22 @@
 class Dado{
     constructor(){
+    }
         tirar(){
             return Math.ceil(Math.random() * 6);
         }
+}
+class Jugador{
+    constructor(nombre){
+        this.nombre = nombre
+        this.posicion = 0
+        this.avance = 0
+    }
+    avanzar(numero){
+        this.avance = numero + this.posicion
+    }
+}
+class Tablero{
+    casillas(numero){
+        
     }
 }
